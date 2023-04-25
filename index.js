9d9f0d4e4d46a2e0a81df6f650cf48449014e976
patrick-francke <patrick.francke@ezs.ch>
2023-04-25 08:54:18 +0200

    Added Dockerfile and API endpoint

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const { exec } = require('child_process');
+const fs = require('fs');
+
+const app = express();
+
+//for pseudo-query sanitization
+function removeSpecialCharacters(str) {
+  return str.replace(/[^a-zA-Z0-9-_:\/%&=]/g, '');
+}
+
+//provide api endpoint
+app.get('/generate-pdf', (req, res) => {
+  
+console.log(req.query.dashboardUrl);
+  const dashboardUrl = removeSpecialCharacters(decodeURIComponent(req.query.dashboardUrl));
+  const token = req.query.token;
+
+  const d = new Date();
+  const dTime = d.getTime();
+  const filename = req.query.filename
+
+  const outputFilePath = `./output/${filename}_${dTime}.pdf`;
+
+  const command = `node grafana_pdf.js ${dashboardUrl} ${token} ${outputFilePath}`;
+  exec(command, (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error generating PDF');
+      return;
+    }
+
+    console.log(`PDF generated at ${outputFilePath}`);
+
+    var file = fs.createReadStream(outputFilePath);
+    var stat = fs.statSync(outputFilePath);
+    res.setHeader('Content-Length', stat.size);
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename='+filename+'.pdf');
+
+    file.on('end', function() {
+      fs.unlink(outputFilePath, function() {
+        // file deleted
+      });
+    });
+
+    file.pipe(res);
+
+    //res.send('PDF generated');
+  });
+});
+
+app.listen(3001, () => {
+  console.log('Server started');
+});
